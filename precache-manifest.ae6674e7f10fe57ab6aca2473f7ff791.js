e39cb7f130e8acc99ffbb791e2b0937c7acdee1e
Jacob Haugen <howagain@gmail.com>
2020-07-24 19:14:47 -0400

    Updates

---
+++ precache-manifest.ae6674e7f10fe57ab6aca2473f7ff791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf5e04a069858711b1680b6a652ee3e",
+    "url": "/howagain/fcc-quote-machine/index.html"
+  },
+  {
+    "revision": "257000fcda190c843e5d",
+    "url": "/howagain/fcc-quote-machine/static/css/main.aaa5f7d0.chunk.css"
+  },
+  {
+    "revision": "e6caf79fe301a47d4841",
+    "url": "/howagain/fcc-quote-machine/static/js/2.6b6d5c23.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/howagain/fcc-quote-machine/static/js/2.6b6d5c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "257000fcda190c843e5d",
+    "url": "/howagain/fcc-quote-machine/static/js/main.a57f9799.chunk.js"
+  },
+  {
+    "revision": "dbc8c1b8531f95effc59",
+    "url": "/howagain/fcc-quote-machine/static/js/runtime-main.fb70d744.js"
+  }
+]);
